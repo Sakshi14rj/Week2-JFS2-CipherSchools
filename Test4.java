7489850c7922d773558fe210be49ea2f7eab2e2b
Sakshi <78091453+Sakshi14rj@users.noreply.github.com>
2023-02-02 19:06:18 +0530

    Create Test4.java

---
+++ Test4.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Test4{
+    
+    public static void main(String[]args) throws IOException{
+        file f =new File("d:\\ABC.txt");
+        f.createNewFile();
+    }
+}
