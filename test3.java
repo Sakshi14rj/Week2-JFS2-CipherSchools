bb4efa853ca1e303ee94bec40070cb2e64e6c76a
Sakshi <78091453+Sakshi14rj@users.noreply.github.com>
2023-02-02 19:14:36 +0530

    Create test3.java

---
+++ test3.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+public class Test3 {
+	public static void main(String[] args) {
+		// HashSet
+		HashSet myset = new HashSet();
+		myset.add("green");
+		myset.add("red");
+		myset.add("orange");
+		myset.add("red");
+		myset.add("orange");
+		
+		
+		System.out.println(myset);
+		LinkedHashSet lhs = new LinkedHashSet();
+		
+		lhs.add("green");
+		lhs.add("red");
+		lhs.add("orange");
+		lhs.add("red");
+		lhs.add("orange");
+		System.out.println(lhs);
+		
+		TreeSet ts = new TreeSet();
+		ts.add("cat");
+		ts.add("Apple");
+		ts.add("Ball");
+		System.out.println(ts);
+		
+		
+		
+	}
+
+}
