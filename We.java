587e037d65cfa2ee46f71c79271ddcad7302fa82
Sakshi <78091453+Sakshi14rj@users.noreply.github.com>
2023-02-02 19:03:32 +0530

    Create We.java

---
+++ We.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class We{
+    int n1,n2,result;
+    
+    Scanner sc = new Scanner(System.in);
+     void Calc(){
+         try{
+             System.out.println("Enter 1st num");
+             n1 = sc.nextInt();
+             System.out.println("Enter 2st num");
+             n2 = sc.nextInt();
+             result = n1/n2;
+         }
+         catch(Exception e){
+         }
+         System.out.println(result);
+     }
+     public static void main(String[] args){
+         We obj = new We();
+         obj.Calc();
+     }
+}
